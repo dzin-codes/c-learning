1f15ac321e7ad951058f141a3678b8c5b92a1dbb
dzin-codes <dzincodes@gmail.com>
2026-06-21 13:40:51 +0530

    Done

---
+++ CH2/divisible.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    int no;
+    printf("Enter the number: ");
+    scanf("%d",&no);
+
+
+    printf("Zero = Divisible\n");
+    printf("Non-Zero = Not Divisible\n");
+
+    printf("The Result of divisible number is: %d",97%no);
+
+
+    return 0;
+}
